86165fb6e2d8edd26c2e4d5fc33023e832dba113
Beatriz Alves <55098795+bea3853@users.noreply.github.com>
2026-02-26 17:23:36 -0300

    Add files via upload

---
+++ LINGUAGEM PYTHON/dados_primitivos.py
@@ -0,0 +1,23 @@
+# dados primitivos
+# em python
+
+# tipo string -  texto
+
+'texto'
+"texto 2 dhahdkjhasjk"
+
+# tipo interger - inteiro
+
+print(1  +  2)
+2
+100
+
+# tipo float  - real
+
+print(5.0 - 102.10)
+100.
+0.5
+
+# tipo bool  -  lógico
+True
+False
